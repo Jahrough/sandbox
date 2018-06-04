f322fe6325601d04d3781db91748670392f30c39
Jahrough Lopez <jlopez@Jahroughs-iMac.local>
2018-06-04 01:17:33 -0400

    added sass loaders to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,32 @@ const path = require('path');
 const webpack = require('webpack');
 
  module.exports = {
-     entry: './js/main.js',
+     entry: './src/components/app/app.js',
+     mode: 'development',
+     devtool: 'source-map',
      output: {
          path: path.resolve(__dirname, 'build'),
          filename: 'main.bundle.js'
      },
      module: {
         rules: [
-          { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+          { 
+              test: /\.js$/, 
+              exclude: /node_modules/, 
+              loader: "babel-loader" 
+          },
+          { 
+            test: /\.scss$/, 
+            exclude: /node_modules/, 
+            use: [ 
+                { loader: "style-loader" },
+                { loader: "css-loader" },
+                { loader: "sass-loader" }
+            ] 
+          }
         ]
       },
      stats: {
          colors: true
-     },
-     devtool: 'source-map'
+     }
  };
