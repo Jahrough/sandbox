20f0364452e02f8a19facf62f1f554de6c7e05fd
jlopez <Jahrough.Lopez@turner.com>
2018-06-12 22:14:38 -0400

    add base, dev, prod & webpack config...

---
+++ webpack/dev.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+const baseConfig = require('./base.config.js');
+
+// Plugins
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// Settings
+const dist = path.resolve(__dirname, 'build');
+
+module.exports = {
+	entry: [
+		'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+		'./src/components/app/app.js'
+	],
+	mode: 'development',
+	context: __dirname,
+    devtool: 'inline-source-map',
+	output: {
+		path: dist,
+		publicPath: '/',
+		filename: 'main.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'eslint-loader'
+			},
+			{
+				test: /\.(scss|sass)$/,
+				exclude: /node_modules/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				})
+			}
+		]
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	],
+	stats: {
+		colors: true
+	}
+};
---
+++ webpack/prod.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+
+// Plugins
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+// Settings
+const baseConfig = require('./base.config.js');
+const dist = path.resolve(__dirname, 'build');
+
+module.exports = {
+	entry: [
+		'./src/components/app/app.js'
+	],
+	mode: 'production',
+	context: __dirname,
+	output: {
+		path: dist,
+		publicPath: '/',
+		filename: 'main.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'eslint-loader'
+			},
+			{
+				test: /\.(scss|sass)$/,
+				exclude: /node_modules/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				})
+			}
+		]
+	},
+	plugins: [
+        new UglifyJsPlugin({
+            sourceMap: true,
+            include: /\/src/,
+            exclude: /\/node_modules/
+		})
+	],
+	stats: {
+		colors: true
+	}
+};
